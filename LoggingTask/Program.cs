44c1c2f755b243f6f3e1075edc6ca2a7a80ad019
levashov_92 <levashov_92@outlook.com>
2021-06-09 22:07:03 +0300

    finally check

--- LoggingTask/Program.cs
+++ LoggingTask/Program.cs
@@ -34,18 +34,12 @@ namespace LoggingTask
             algorithm.Calculate(100000, 245000, null, logger);
 
             //Algorithm algorithm1 = new Algorithm(new EuclideanAlgorithm(),loggerFactory);
-            //Algorithm algorithm2 = new Algorithm(new SteinAlgorithm(), loggerFactory2);
-            //Algorithm algorithm3 = new Algorithm(new SteinAlgorithm(), loggerFactory3);
-
-
 
             //algorithm1.GcdLogging(0,0);
             //algorithm2.GcdLogging(int.MinValue, 12);
             //algorithm2.GcdLogging(14, 12);
 
-            Console.ReadKey();
-
-        
+            Console.ReadKey();        
         }
     }
 }
